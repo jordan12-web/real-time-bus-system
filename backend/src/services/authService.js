7e2847d1782d39cc853d506008bf771fa7c29f49
Yordanos Molla <yordanosmolla12@gmail.com>
2026-08-14 14:59:43 +0300

    bcrypt fix

--- backend/src/services/authService.js
+++ backend/src/services/authService.js
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import User from '../models/User.js';
 import { signAccessToken, signRefreshToken, verifyRefreshToken } from '../utils/jwt.js';
 
